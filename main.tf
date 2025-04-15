64a4f0eb77a343369fd4767a43d643cc0595236f
Douglas Garstang <dgarstang@192-168-1-113.tpgi.com.au>
2025-04-15 11:36:06 +1000

    ...

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 data "local_file" "private_key" {
-  filename = "${path.cwd}/private.key"
+  filename = "${file("${path.module}/private.key")}"
 }
 
 resource "null_resource" "write_wireguard_config" {
